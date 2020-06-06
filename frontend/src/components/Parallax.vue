5bb543f44aff3310d3d9e002e7f4443c7f38dfd0
PAWEL-Sam <pawel_sam@mail.ru>
2020-06-06 18:21:50 +0300

    исправление ошибок в шкале #2

--- frontend/src/components/Parallax.vue
+++ frontend/src/components/Parallax.vue
@@ -1,25 +1,26 @@
 <template>
-    <main class="parallax-container"  @wheel.passive="wheelScrollingOffset">
-    <TagLayer :transmittedWidth = "scaleFullWidth" :offsetParallax = "offsetParallax" :order = "1"/>
-    <div class="parallax-layer-2 parallax-layer parallax_container_level-2" :style="{width: scaleFullWidth, transform: 'translateX(' + offsetParallax*0.6 + 'px)'}">
-      <scale
-      :startDate="2010"
-      :endDate="2028"
-      :step="1"
-      :ratio="1.0"
-    />
+  <main class="parallax-container" @wheel.passive="wheelScrollingOffset">
+    <TagLayer :transmittedWidth="scaleFullWidth" :offsetParallax="offsetParallax" :order="1" />
+    <div
+      class="parallax-layer-2 parallax-layer parallax_container_level-2"
+      :style="{width: scaleFullWidth, transform: 'translateX(' + offsetParallax * 0.6 + 'px)'}"
+    >
+      <scale :startDate="2010" :endDate="2028" :step="1" :ratio="1.0" />
     </div>
-    <div class="parallax-layer-1 parallax-layer parallax_container_level-1" :style="{width: scaleFullWidth, transform: 'translateX(' + offsetParallax + 'px)'}">
+    <div
+      class="parallax-layer-1 parallax-layer parallax_container_level-1"
+      :style="{width: scaleFullWidth, transform: 'translateX(' + offsetParallax + 'px)'}"
+    >
       <scale
-      v-on:returnScaleLength = "getScrollLength"
-      :startDate="2010"
-      :endDate="2028"
-      :step="1"
-      :ratio="1.0"
-    />
+        v-on:returnScaleLength="getScrollLength"
+        :startDate="2010"
+        :endDate="2028"
+        :step="1"
+        :ratio="1.0"
+      />
     </div>
-<scroller :scrollDummyWidth="scaleLength"  v-on:offsetX = "scrollParallax" />
-<editor/>
+    <scroller :scrollDummyWidth="scaleLength" v-on:offsetX="scrollParallax" />
+    <editor />
   </main>
 </template>
 
@@ -41,9 +42,9 @@ export default {
   data() {
     return {
       scale: {},
-      scaleLength: 35,
+      scaleLength: 0,
       offsetParallax: 0,
-      tagsData: null,
+      tagsData: null
       }
   },
   created() {
@@ -51,7 +52,7 @@ export default {
   },
   computed: {
     scaleFullWidth: function () {
-      return  this.scaleLength + 100 +'px'
+      return  this.scaleLength + 'px'
     }
   },
   methods: {
@@ -59,34 +60,33 @@ export default {
       this.scaleLength = length + 100
     },
     scrollParallax(data) {
-      this.offsetParallax = data - 20;
+      this.offsetParallax = data - 0;
     },
      wheelScrollingOffset(event) {
        this.$root.$emit('wheelScroll', event.deltaY)
     },
-    }
+  }
 }
 </script>
 
 <style lang="scss">
-   
 .parallax-container {
-    height: 100%;
-    grid-row-start: 2;
-    grid-row-end: 3;
-    grid-column-start: 1;
-    grid-column-end: 2;
-    width: 100vw;
-    display: grid;
-    grid-template-rows: 1fr 1fr;
-    grid-template-columns: 100%;
+  height: 100%;
+  grid-row-start: 2;
+  grid-row-end: 3;
+  grid-column-start: 1;
+  grid-column-end: 2;
+  width: 100vw;
+  display: grid;
+  grid-template-rows: 1fr 1fr;
+  grid-template-columns: 100%;
 }
 .parallax-layer {
-    text-align: center;
-    display: flex;
-    align-items: flex-end;
-    overflow: hidden;
-    z-index: 2;
+  text-align: center;
+  display: flex;
+  align-items: flex-end;
+  overflow: hidden;
+  z-index: 2;
 }
 .parallax-layer-2 {
   background-image: url("../images/skale.svg");
--- frontend/src/components/Scale.vue
+++ frontend/src/components/Scale.vue
@@ -1,142 +1,126 @@
 <template>
-    <div>
-        <svg
-                :width="xEnd+50"
-                height="50%"
-        >
+  <div>
+    <svg :width="xEnd+50" height="50%">
+      <line :x1="x0" y1="80" :x2="xEnd" y2="80" style="stroke:rgb(0,0,0); stroke-width:2" />
 
-            <line
-                    :x1="x0"
-                    y1="80"
-                    :x2="xEnd"
-                    y2="80"
-                    style="stroke:rgb(0,0,0);stroke-width:2"/>
+      <line
+        v-for="line in marksArr"
+        :key="line.x"
+        :x1="line.x"
+        y1="80"
+        :x2="line.x"
+        :y2="line.y"
+        style="stroke:rgb(0,0,0); stroke-width:1"
+      />
 
-            <line
-                    v-for="line in marksArr"
-                    :key="line.x"
-                    :x1="line.x"
-                    y1="80"
-                    :x2="line.x"
-                    :y2="line.y"
-                    style="stroke:rgb(0,0,0);stroke-width:1"/>
-
-            <text
-                    v-for="(mark, index) in signsArr"
-                    :key="index"
-                    :x="mark.x"
-                    y="95"
-                    fill="black"
-            >
-                {{ mark.value }}
-            </text>
-
-        </svg>
-    </div>
+      <text
+        v-for="(mark, index) in signsArr"
+        :key="index"
+        :x="mark.x"
+        y="95"
+        fill="black"
+      >{{ mark.value }}</text>
+    </svg>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "Scale",
-        props: {
-            startDate: Number,
-            endDate: Number,
-            step: Number,
-            ratio: Number
-        },
-        data() {
-            return {
-                wpx: window.screen.availWidth,
-                x0: 50,
-                xEnd: 0,
-                marksArr: [],
-                signsArr: [],
-                scaleLength: 0,
-            }
-        },
-
-        created() {
-           this.calculateScale()
-           this.$emit('returnScaleLength', this.scaleLength)
-        },
-
-        mounted() {
-
-        },
-
-        methods: {
-            calculateScale() {
-                console.log('0)', this.wpx); // убрать
-                
-                this.wpx = Math.floor(this.wpx / 100) * 100;  // округление до сотен в меньшую сторону
-
-                this.xEnd = (this.wpx * 1.3) - 50;
-                this.scaleLength = ((this.xEnd - this.x0) * this.ratio)
-                console.log('1)', this.wpx, this.xEnd, this.scaleLength, this.x0); // убрать
-
-                const minMarkStep = (this.wpx / 10);
-                console.log('2)', minMarkStep); // убрать
-
-                let markQnt = ((this.endDate - this.startDate) / this.step);
-                let markStep = (this.scaleLength / markQnt);
-                console.log('3)', markQnt, markStep); // убрать
-                console.log('4)', markStep, ' >?< ', minMarkStep); // убрать
-
-                if (markStep < minMarkStep) {
-                    this.xEnd = minMarkStep * markQnt;
-                    this.scaleLength = ((this.xEnd - this.x0) * this.ratio)
-                    markStep = minMarkStep;                    
-                }
-                console.log('5)', markQnt, ' лет; конец: ', this.xEnd, '; сама шкала: ', this.scaleLength); // убрать
-
-                let sign = this.startDate;
-                let bigMark = 12;
-                if (this.step !== 1) {
-                    bigMark = this.step
-                }
-
-                let smallMarkQnt = 12 * markQnt;
-                if (this.step !== 1) {
-                    smallMarkQnt = this.step * markQnt
-                }
-                console.log('6)', bigMark, 'месяцев x ', markQnt, 'лет = ', smallMarkQnt); // убрать
-
-                const shift = ((this.xEnd - this.x0) * (1 - this.ratio) / 2)               
-                this.x0 += shift
-                //console.log('7)', shift, this.x0); // убрать
-
-                const smallMArkStep = Math.floor(this.scaleLength / smallMarkQnt)
-                console.log('8)', smallMArkStep, ' округленный шаг между мелкими рисками'); // убрать
-
-                this.scaleLength = (smallMArkStep * smallMarkQnt) // пересчитаем scaleLength
-                console.log('9)', this.scaleLength, ' новый размер самой шкалы = ', smallMArkStep, 'ширина деления х ', smallMarkQnt, 'кол-во рисок', ' + 100 = ', this.scaleLength + 100, ' scaleFullWidth'); // убрать
-                
-                this.xEnd = this.x0 + this.scaleLength // пересчитаем конец шкалы
-                console.log('10)', this.x0, this.xEnd); // убрать
-
-                let counter = 0;
-                for (let x = this.x0; x <= this.xEnd; x += smallMArkStep) {
-                    if (counter % bigMark === 0) {
-                        this.marksArr.push({x, y: 30})
-                        this.signsArr.push({
-                            x: x - 17,
-                            value: String(sign)
-                        })
-                        sign += this.step                        
-                    }
-                    else {
-
-                        this.marksArr.push({x, y: 60})
-                    }
-                    counter++
-                }                
-                console.log('11)', this.marksArr); // убрать
-            }
-        }
-
+export default {
+  name: "Scale",
+  props: {
+    startDate: Number,
+    endDate: Number,
+    step: Number,
+    ratio: Number
+  },
+  data() {
+    return {
+      wpx: window.screen.availWidth,
+      x0: 50,
+      xEnd: 0,
+      marksArr: [],
+      signsArr: [],
+      scaleLength: 0
     };
+  },
 
+  created() {
+    this.calculateScale();
+    this.$emit("returnScaleLength", this.scaleLength);
+  },
 
+  mounted() {},
+
+  methods: {
+    calculateScale() {
+      //console.log("0)", this.wpx, " px"); // убрать
+      this.wpx = Math.floor(this.wpx / 100) * 100; // округление до сотен в меньшую сторону
+
+      this.xEnd = this.wpx * 1.3 - 50;
+      this.scaleLength = (this.xEnd - this.x0) * this.ratio;
+      //console.log("1)", this.wpx, this.xEnd, this.scaleLength, this.x0); // убрать
+
+      let minMarkStep = this.wpx / 10;
+      if (minMarkStep < 130) {
+        minMarkStep = 130;
+      }
+      //console.log("2)", minMarkStep, "px это ~ 1/10 экрана"); // убрать
+
+      let markQnt = (this.endDate - this.startDate) / this.step;
+      let markStep = Math.floor(this.scaleLength / markQnt);
+      //console.log("3)", markQnt, "лет", markStep); // убрать
+      //console.log("4)", markStep, " >?< ", minMarkStep); // убрать
+
+      if (markStep < minMarkStep) {
+        markStep = minMarkStep;
+      }
+      this.scaleLength = markStep * markQnt;
+      this.xEnd = this.scaleLength + this.x0;
+      //console.log("5)", markQnt, " лет. Шкала:", this.scaleLength, "; конец: ", this.xEnd); // убрать
+
+      let sign = this.startDate;
+      let bigMark = 12;
+      if (this.step !== 1) {
+        bigMark = this.step;
+      }
+
+      let smallMarkQnt = 12 * markQnt;
+      if (this.step !== 1) {
+        smallMarkQnt = this.step * markQnt;
+      }
+      //console.log("6)", bigMark, "месяцев x ", markQnt, "лет = ", smallMarkQnt, " рисок"); // убрать
+
+      const shift = ((this.xEnd - this.x0) * (1 - this.ratio)) / 2;
+      this.x0 += shift;
+      //console.log('7)', shift, this.x0); // убрать
+
+      const smallMArkStep = Math.floor(this.scaleLength / smallMarkQnt);
+      //console.log("8)", smallMArkStep, "px - округленный шаг между мелкими рисками"); // убрать
+
+      this.scaleLength = smallMArkStep * smallMarkQnt; // пересчитаем scaleLength
+      //console.log("9)", this.scaleLength, "px новый размер самой шкалы = ", smallMArkStep, "px между рисками х ", smallMarkQnt, "кол-во рисок", " + 2*50 = ", this.scaleLength + 100, " scaleFullWidth"); // убрать
+
+      this.xEnd = this.x0 + this.scaleLength; // пересчитаем конец шкалы
+      //console.log("10)", this.x0, this.xEnd, "начало и конец самой шкалы без отступов"); // убрать
+
+      let counter = 0;
+      for (let x = this.x0; x <= this.xEnd; x += smallMArkStep) {
+        if (counter % bigMark === 0) {
+          this.marksArr.push({ x, y: 30 });
+          this.signsArr.push({
+            x: x - 17,
+            value: String(sign)
+          });
+          sign += this.step;
+        } else {
+          this.marksArr.push({ x, y: 60 });
+        }
+        counter++;
+      }
+      //console.log("11)", this.marksArr); // убрать
+    }
+  }
+};
 </script>
 
 <style scoped>
